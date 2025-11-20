7c451cbe1a2b4385545db08b9fba8c5a68ad125d
Ariel Eliahu <ariel.eliahu@post.runi.ac.il>
2025-11-20 17:43:58 +0200

    Completed assignment

--- Anagram.java
+++ Anagram.java
@@ -58,7 +58,7 @@ public class Anagram {
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		return str.toLowerCase().replaceAll("[^a-z]", "");
-	} 
+	}
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
@@ -75,3 +75,4 @@ public class Anagram {
     return new String(c);
 	}
 }
+
